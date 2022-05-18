6c9d34faae58f92c69aef066a64ce4178001e02f
mrcportillo <mrcportillo@users.noreply.github.com>
2022-05-18 17:10:18 -0300

    handle type turns

--- models/Turn.js
+++ models/Turn.js
@@ -1,6 +1,7 @@
 import { nanoid } from 'nanoid';
 
-let positions = 1;
+let general_positions = 0;
+let vip_positions = 0;
 
 class Turn {
   static TURN_TYPES = {
@@ -10,15 +11,23 @@ class Turn {
 
   constructor(type) {
     this.id = nanoid(5);
-    this.position = this.id;
-    this.positions_before = positions++;
-    this.estimated_minutes = positions * 5;
     this.type = type;
     this.date = new Date().toISOString();
+    if (type === Turn.TURN_TYPES.VIP) {
+      this.positions_before = vip_positions;
+      vip_positions++;
+      this.position = vip_positions;
+      this.estimated_minutes = vip_positions * 3;
+    } else {
+      this.positions_before = general_positions;
+      general_positions++;
+      this.position = general_positions;
+      this.estimated_minutes = general_positions * 5;
+    }
   }
 
-  static create(type = 'GENERAL') {
-    return new Turn(type);
+  static create(type = Turn.TURN_TYPES.GENERAL) {
+    return new Turn(type.toUpperCase());
   }
 }
 
