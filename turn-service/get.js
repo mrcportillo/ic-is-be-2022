f3bd7a60c0ebb319fe229d9470ebcf81fd62c69a
mrcportillo <mrcportillo@users.noreply.github.com>
2022-05-18 17:10:09 -0300

    fixed service response

--- turn-service/get.js
+++ turn-service/get.js
@@ -1,34 +1,30 @@
-const { default: Turn } = require('../models/Turn');
+import Turn from '../models/Turn';
 
-module.exports.main = (event) => {
-  const type = event.queryStringParameters.type;
+const HEADERS = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': true,
+};
+
+exports.main = (event, context, callback) => {
+  const type = event.queryStringParameters?.type;
+
   try {
-    const turn = Turn.create(
-      type === Turn.TURN_TYPES.VIP
-        ? Turn.TURN_TYPES.VIP
-        : Turn.TURN_TYPES.GENERAL
-    );
-    return {
+    const turn = Turn.create(type);
+
+    callback(null, {
       statusCode: 200,
-      body: JSON.stringify(
-        {
-          turn,
-        },
-        null,
-        2
-      ),
-    };
+      headers: HEADERS,
+      body: JSON.stringify({ turn }),
+    });
   } catch (e) {
-    return {
+    callback(null, {
       statusCode: 500,
-      body: JSON.stringify(
-        {
-          message: 'Something went worng',
-          input: event,
-        },
-        null,
-        2
-      ),
-    };
+      headers: HEADERS,
+      body: JSON.stringify({
+        message: 'Something went worng',
+        input: event,
+      }),
+    });
   }
 };
