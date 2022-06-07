37509c2b3bb673dad8cf524ee67fc6c572139393
mrcportillo <mrcportillo@users.noreply.github.com>
2022-06-07 19:30:42 -0300

    fixed preferal unit test

--- utils/tests/turn.test.js
+++ utils/tests/turn.test.js
@@ -18,7 +18,7 @@ describe('turn utils', () => {
     });
 
     test('should use PREFERAL time by turn', () => {
-      expect(getEstimatedMinutesByType(Turn.TURN_TYPES.PREFERAL, 1)).toBe(3);
+      expect(getEstimatedMinutesByType(Turn.TURN_TYPES.PREFERAL, 1)).toBe(2);
       expect(getEstimatedMinutesByType(Turn.TURN_TYPES.PREFERAL, 4)).toBe(8);
       expect(getEstimatedMinutesByType(Turn.TURN_TYPES.PREFERAL, 10)).toBe(20);
       expect(getEstimatedMinutesByType(Turn.TURN_TYPES.PREFERAL, 0)).toBe(0);
